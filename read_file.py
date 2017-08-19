ab7cf881ba53fc80621895357cfdae01984b2737
kyle <keboland@calpoly.edu>
2017-08-19 01:32:06 -0400

    Adds for statement which iterates through all objects in lines. If empty removes the iterable from the lines variable

--- read_file.py
+++ read_file.py
@@ -12,6 +12,12 @@ num_words = len(words)				#Creates variable num_words equal to the length of the
 print("The number of words is ", num_words)	#Prints the number of words with the variable num_words
 
 lines = data.split("\n")			#Creates variable lines equal to the each new line in file
+
+for l in lines:					#loops over lines list l will contain each lines as Python loops over them
+	if not l:				#If l is empty (Not Checks for emptiness is whatever is being examined
+		lines.remove(l)			#Remove iterable l from variables lines
+
+
 print("The lines in the test are:")		#Prints Text
 print(lines)					#Prints lines
 print("The number of lines is:",len(lines))	#Prints text and results of length of lines
